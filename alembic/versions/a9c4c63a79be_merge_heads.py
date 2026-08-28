3c0a4dc40d628e95529e864be1da581d4ed6f2b2
Narain-karthick <240146.ad@rmkec.ac.in>
2026-08-28 22:36:42 +0530

    fix: merge alembic migration heads

---
+++ alembic/versions/a9c4c63a79be_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: a9c4c63a79be
+Revises: 003_add_candidate_features, 850f7086ffdd
+Create Date: 2026-08-28 22:35:13.855443
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'a9c4c63a79be'
+down_revision: Union[str, Sequence[str], None] = ('003_add_candidate_features', '850f7086ffdd')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
