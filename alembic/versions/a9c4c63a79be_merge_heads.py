e1534b158c9b3a705764df4617bf2b8498ea796e
Narain-karthick <240146.ad@rmkec.ac.in>
2026-08-28 22:49:22 +0530

    Fix black formatting on merge_heads migration

--- alembic/versions/a9c4c63a79be_merge_heads.py
+++ alembic/versions/a9c4c63a79be_merge_heads.py
@@ -5,6 +5,7 @@ Revises: 003_add_candidate_features, 850f7086ffdd
 Create Date: 2026-08-28 22:35:13.855443
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,11 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a9c4c63a79be'
-down_revision: Union[str, Sequence[str], None] = ('003_add_candidate_features', '850f7086ffdd')
+revision: str = "a9c4c63a79be"
+down_revision: Union[str, Sequence[str], None] = (
+    "003_add_candidate_features",
+    "850f7086ffdd",
+)
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
